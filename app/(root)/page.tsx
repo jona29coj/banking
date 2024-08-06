1357ed5147b1b661b0405922ea1fdc9bdd41d387
Jonathan Prince A <jonacoj29@gmail.com>
2024-08-06 17:14:45 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
-const page = () => {
+const Home = () => {
+    const loggedIn = {firstName:'Jonathan',lastName:'Prince',email:'jonacoj29@gmail.com'}
   return (
-    <div>page</div>
+    <section className='home'>
+        <div className='home-content'>
+        <header className='home-header'>
+            <HeaderBox
+                type='greeting'
+                title='Welcome'
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transactions efficiently."/>
+            <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250}/>
+        </header>
+        </div>
+        <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:123.50},{currentBalance:525.50}]}
+      />
+    </section>
   )
 }
 
-export default page
+export default Home
